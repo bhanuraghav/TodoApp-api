12b5cfe842f8d4402a546f875a8eb08c3987a260
bhanuraghav <bhanuraghav1999@gmail.com>
2018-10-24 01:37:59 +0530

    Update Route Added

--- server/server.js
+++ server/server.js
@@ -1,7 +1,8 @@
-var express = require('express'),
+const express = require('express'),
     bodyParser = require('body-parser');
 
 const {ObjectID} = require('mongodb');
+const _ =require('lodash')
 
 
 var {mongoose} = require('./db/mongoose');
@@ -87,6 +88,30 @@ app.delete('/todos/:id',(req,res)=>{
     }).catch((e)=>{
         res.status(400).send();
     })
+    
+});
+
+app.patch('/todos/:id',(req,res)=>{
+    var id = req.params.id;
+    var body = _.pick(req.body,['text','completed']);
+    if(!ObjectID.isValid(id)){
+        return res.status(404).send();
+    }
+    if(_.isBoolean(body.completed)&& body.completed){
+        body.completedAt = new Date().getTime();
+   }else{
+       body.completed =false;
+       body.completedAt =null;
+   }
+
+   Todo.findByIdAndUpdate(id,{$set: body},{new : true}).then((todo)=>{
+        if(!todo){
+            return res.status(404).send();  
+        }
+        res.send({todo});
+   }).catch((e)=>{
+       res.status(400).send();
+   })
     
 })
 
