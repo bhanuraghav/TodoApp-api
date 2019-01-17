1fdfd44761367fd6416f697604e2c4b9558f620b
bhanuraghav <bhanuraghav1999@gmail.com>
2019-01-17 22:49:41 +0530

    Add POST users  login route

--- server/server.js
+++ server/server.js
@@ -130,7 +130,17 @@ app.post('/users',(req,res)=>{
 
 app.get('/users/me',authenticate,(req,res)=>{
   res.send(req.user);
+})
 
+app.post('/users/login',(req,res)=>{
+    var body = _.pick(req.body,['email','password']);
+    User.findByCredentials(body.email,body.password).then((user)=>{
+       user.generateAuthToken().then((token)=>{
+           res.header('x-auth',token).send(user);
+       });
+    }).catch((e)=>{
+        res.status(400).send();
+    })
 })
 
 
