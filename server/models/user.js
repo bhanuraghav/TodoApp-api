3daf40d689ace0d9bef865bae080e7efdf541789
bhanuraghav <bhanuraghav1999@gmail.com>
2019-01-17 22:18:33 +0530

    Hash user password added

--- server/models/user.js
+++ server/models/user.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const jwt = require('jsonwebtoken');
 const validator = require('validator');
 const _ = require('lodash');
+const bcrypt = require('bcryptjs');
 
 var UserSchema =  new mongoose.Schema({
     email: {
@@ -79,6 +80,21 @@ UserSchema.statics.findByToken = function(token){
     })
 
 }
+
+UserSchema.pre('save',function(next){
+    var user = this;
+
+    if(user.isModified('password')){
+        bcrypt.genSalt(10,(err,salt)=>{
+            bcrypt.hash(user.password,salt,(err,hash)=>{
+                user.password = hash;
+                next();
+            })
+        })
+    } else{
+        next();
+    }
+})
 
 
 
