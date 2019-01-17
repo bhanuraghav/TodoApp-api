6fe7665f00ab1d17a8388417ad8e9c2b39955c36
bhanuraghav <bhanuraghav1999@gmail.com>
2019-01-17 20:22:02 +0530

    Authenticcation through middleware

--- server/server.js
+++ server/server.js
@@ -9,6 +9,7 @@ const _ =require('lodash')
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
+var {authenticate} = require('./middleware/authenticate');
 
 var app = express();
 // const port = process.env.PORT || 3000;
@@ -125,6 +126,13 @@ app.post('/users',(req,res)=>{
         res.status(400).send(e);
     })
 })
+
+
+app.get('/users/me',authenticate,(req,res)=>{
+  res.send(req.user);
+
+})
+
 
 app.listen(port,()=>{
     console.log(`Started on port ${port}`);
