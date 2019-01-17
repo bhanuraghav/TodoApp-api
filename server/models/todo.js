42d5797ce754ed8f45975c87f9116a576adf0bcb
bhanuraghav <bhanuraghav1999@gmail.com>
2019-01-18 03:29:38 +0530

    Routes Authenticated

--- server/models/todo.js
+++ server/models/todo.js
@@ -15,7 +15,12 @@ var Todo = mongoose.model('Todo',{
     completedAt:{
         type : Number,
         default : null
+    },
+    _creator : {
+        type: mongoose.Schema.Types.ObjectId,
+        required : true
     }
+
 });
 
 module.exports = {
--- server/server.js
+++ server/server.js
@@ -17,9 +17,10 @@ const port = process.env.PORT;
 
 app.use(bodyParser.json());
 
-app.post('/todos',(req,res)=>{
+app.post('/todos',authenticate,(req,res)=>{
     var todo = new Todo({
-        text: req.body.text
+        text: req.body.text,
+        _creator : req.user._id
     });
     todo.save().then((doc)=>{
         res.send(doc);
@@ -29,8 +30,10 @@ app.post('/todos',(req,res)=>{
    // console.log(req.body);
 })
 
-app.get('/todos',(req,res)=>{
-    Todo.find().then((todos)=>{
+app.get('/todos',authenticate,(req,res)=>{
+    Todo.find({
+        _creator : req.user._id
+    }).then((todos)=>{
         res.send({todos});
     },(e)=>{
         res.status(400).send(e);
@@ -43,7 +46,7 @@ app.get('/todos',(req,res)=>{
 })
 
 //GET /todos/_id
-app.get('/todos/:id',(req,res)=>{
+app.get('/todos/:id',authenticate,(req,res)=>{
     var id = req.params.id;
     //Validate id using isValid
         //404 -send back empty send
@@ -51,7 +54,11 @@ app.get('/todos/:id',(req,res)=>{
         return res.status(404).send();
     }
     
-    Todo.findById(id).then((todo)=>{
+    Todo.findOne({
+        _id : id,
+        _creator : req.user._id
+         
+    }).then((todo)=>{
         if(!todo){
             return res.status(404).send();           
         }
@@ -64,7 +71,7 @@ app.get('/todos/:id',(req,res)=>{
 
 //Delete Route
 
-app.delete('/todos/:id',(req,res)=>{
+app.delete('/todos/:id',authenticate,(req,res)=>{
     //get the id
     var id = req.params.id;
 
@@ -80,7 +87,10 @@ app.delete('/todos/:id',(req,res)=>{
         //error
             //400 with empty body
 
-    Todo.findByIdAndDelete(id).then((todo)=>{
+    Todo.findOneAndDelete({
+        _id : id,
+        _creator : req.user._id
+    }).then((todo)=>{
         if(!todo){
             return res.status(404).send();
         }
@@ -91,7 +101,7 @@ app.delete('/todos/:id',(req,res)=>{
     
 });
 
-app.patch('/todos/:id',(req,res)=>{
+app.patch('/todos/:id',authenticate,(req,res)=>{
     var id = req.params.id;
     var body = _.pick(req.body,['text','completed']);
     if(!ObjectID.isValid(id)){
@@ -104,7 +114,10 @@ app.patch('/todos/:id',(req,res)=>{
        body.completedAt =null;
    }
 
-   Todo.findByIdAndUpdate(id,{$set: body},{new : true}).then((todo)=>{
+   Todo.findOneAndUpdate({
+        _id : id,
+        _creator : req.user._id
+   },{$set: body},{new : true}).then((todo)=>{
         if(!todo){
             return res.status(404).send();  
         }
